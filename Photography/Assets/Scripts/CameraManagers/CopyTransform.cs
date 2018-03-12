38ff1f76d6381157bab49049cb24b1622e9194f7
matiasup <matias.selzer@gmail.com>
2018-03-12 19:47:48 -0300

    ARCamera changed
    The ARCamera was changed by a normal camera which follows the ARCamera's position and rotation. Hence, the zoom works now.

---
+++ Photography/Assets/Scripts/CameraManagers/CopyTransform.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyTransform : MonoBehaviour {
+
+	public Transform other;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Slerp(transform.position, other.position,10*Time.deltaTime);
+		transform.rotation = Quaternion.Lerp(transform.rotation, other.rotation,10*Time.deltaTime) ;
+	}
+}
